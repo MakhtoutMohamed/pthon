4282ab687db28c1521ebd46ea278f6db5e73205d
makhtout <32824512+medmakhtout@users.noreply.github.com>
2022-04-19 02:53:14 +0000

    update math.py

--- math.py
+++ math.py
@@ -1,7 +1,7 @@
 
 #let's do math
 
-print("what's the answer to life, the universe, and everything?", 40+20-10*2)
+print("what's the answer to life, the universe, and everything?", 40+20-10*2/3)
 print("is it true that 1+1*2 equal 3")
 print(1+1*2)
 print("you right it is")
