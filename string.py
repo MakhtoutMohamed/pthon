78a60e5a373a6657653ad3f0cc38988ecef15b95
makhtout <32824512+medmakhtout@users.noreply.github.com>
2022-04-19 03:05:44 +0000

    add f on the print function

---
+++ string.py
@@ -0,0 +1,17 @@
+# strings are text surrounded by quotes
+
+
+name = "alice"
+phrase = "i love everyone and"
+
+print(phrase, name)
+
+############## nothing
+
+name = "Casablanca"
+number = 200
+float = 123.21
+
+total = number + float
+
+print(f"{name} the total of people there is: {total:.3f}")
