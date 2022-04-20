375f803369ae4e972bf55846b430735169797be9
makhtout <32824512+medmakhtout@users.noreply.github.com>
2022-04-20 18:07:17 +0000

    fizzbuzz challenge

--- square.py
+++ square.py
@@ -3,5 +3,24 @@ import os
 
 os.system("clear")
 
-for i in range(11):
+for i in range(5):
     print(f"the square of {i} is: ", i*i)
+
+###################################
+random_things = ["Puppies", 55, "Anthony Jonh", 1/2, ["Oh", "yes", "list inside a list"]]
+# here's how you access element of a list
+
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+first_animal = animals[0]
+print(first_animal)
+third_animal = animals[2]
+print(third_animal)
+
+print("There are this many things:", len(random_things))
+print("things is a:", type(random_things))
+
+another_list = random_things[-1]
+print("the last element: ", another_list)
+another_list = random_things[0]
+print("the first element: ", another_list)
+print(type(another_list))
