0bfe1ec26061dc4d58a1ab9e0eb0f8b3867a8fd4
makhtout <32824512+medmakhtout@users.noreply.github.com>
2022-04-21 22:22:01 +0000

    new file functions.py

---
+++ functions.py
@@ -0,0 +1,17 @@
+
+def greet(name):
+    return f"Hey {name}!"
+
+def concatenate(word_one, word_two):
+    return word_one + word_two
+
+def age_on_dog_years(age):
+    result = age * 7
+    return result
+
+print(greet('matan'))
+print(greet('chris'))
+
+print(concatenate('matan ', 'chris'))
+
+print(age_on_dog_years(28))
