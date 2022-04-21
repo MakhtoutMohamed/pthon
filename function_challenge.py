295b52861df707f9c1fd0220792feb712ce157f4
makhtout <32824512+medmakhtout@users.noreply.github.com>
2022-04-21 23:16:08 +0000

    challenge function

---
+++ function_challenge.py
@@ -0,0 +1,7 @@
+# define a function with parammetre and get this one uppercase and reversed
+
+def uppercase_and_reverse(a):
+    # result = a.upper() [::-1]
+    return a.upper() [::-1]
+
+print(uppercase_and_reverse("banana movies"))
