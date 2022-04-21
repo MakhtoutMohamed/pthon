4f86c8c383587c0d4b041462b29609017a5fbf08
makhtout <32824512+medmakhtout@users.noreply.github.com>
2022-04-21 20:22:30 +0000

    dictionary and lists

--- dictionary.py
+++ dictionary.py
@@ -31,3 +31,22 @@ blog_post = {'title': 'the best language over the world', 'day': 'the first day'
 
 print(user['name'])
 print(blog_post['title'])
+
+# dictionary and lists can be inside of each other
+
+animal_sounds = {
+    "cat": ["meow", "purr"],
+    "dog": ["woof", "bark"],
+    "fox": []
+}
+
+matan = {'name': 'matan', 'height': 70, 'shoe size': 10.5, 'hair': 'black', 'eyes': 'blue'}
+chris = {'name': 'chris', 'height': 67, 'shoe size': 10, 'hair': 'brown', 'eyes': 'blue'}
+sarah = {'name': 'sarah', 'height': 61, 'shoe size': 8, 'hair': 'black', 'eyes': 'black'}
+
+people = [matan, chris, sarah]
+print(people)
+
+for person in people:
+    # print(person.get('height', 'none'))
+    print(person['height'])
